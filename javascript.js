89d63b2a0b31c717fb19b456f06f1b474cf1c371
naomichoe <114212195+naomichoe@users.noreply.github.com>
2022-11-29 01:44:01 -0800

    created a new branch to add js for coloring the grid

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
+const resizeBtn = document.querySelector(".resize-btn");
+
 //create divs for the board
 const createGrid = function(num) {
-    let correctGridNum = num * num;
-    let gridContainer = document.querySelector('.grid-container');
+    const correctGridNum = num * num;
+    const gridContainer = document.querySelector('.grid-container');
     gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
-    
-    while (correctGridNum > 0) {
-        let newDiv = gridContainer.appendChild(document.createElement('div'));
-        newDiv.setAttribute('class', 'grid')
-        correctGridNum--;
+ 
+    for (let i = 0; i < correctGridNum; i++) {
+        const newDiv = gridContainer.appendChild(document.createElement('div'));
+        newDiv.setAttribute('class', 'grid');
+        newDiv.addEventListener('mouseover', changeColor);
     }
 }
 
 createGrid(16);
 
-// // mouseover event
+function changeColor(e) {
+    e.target.style.backgroundColor = "black";
+}
 
